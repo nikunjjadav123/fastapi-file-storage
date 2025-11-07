2b1f24d9b88598d0e8e35f6fb28267447a28c607
nikunjjadav123 <dirensj@gmail.com>
2025-11-07 19:58:58 +0530

    Updated main.py file

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.file_manager import save_file, list_files
 from app.config import UPLOAD_DIR
 import os
 
-app = FastAPI(title = "File Storage Service")
+app = FastAPI(title = "File Storage Service123")
 
 @app.post("/upload", response_model=FileSchema)
 async def upload_file(file: UploadFile = File(...)):
