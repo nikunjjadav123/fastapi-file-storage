99e5abd25bd821acd5be588aa19a34c25cc6548a
nikunjjadav123 <dirensj@gmail.com>
2025-11-08 19:23:37 +0530

    Updated FileSchema in schemas.py file

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,8 @@
 from pydantic  import BaseModel, constr, field_validator
 
 class FileSchema(BaseModel):
-    filename: constr(strip_whitespace=True, min_length=3, max_length=255)
-    url: str
+    filename: str
+    url: str | None = None
     size: int | None = None
     type: str | None = None
 
@@ -14,10 +14,4 @@ class FileSchema(BaseModel):
             raise ValueError("Only image files are allowed")
         return value
     
-    @field_validator("size")
-    @classmethod
-    def check_file_size(cls, value):
-        max_size = 2 * 1024 * 1024  # 2 MB
-        if value > max_size:
-            raise ValueError("File size exceeds 2 MB limit")
-        return value
+    
