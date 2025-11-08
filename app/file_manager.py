560abbb0d4f63a3eb5ea5df2aeca899e2627aa43
nikunjjadav123 <dirensj@gmail.com>
2025-11-08 16:04:57 +0530

    File validation ( type and size ) logic implemented

--- app/file_manager.py
+++ app/file_manager.py
@@ -1,12 +1,34 @@
 import os
-from fastapi import UploadFile
+from fastapi import HTTPException, UploadFile
+import magic
 from app.config import UPLOAD_DIR
+from app.schemas import FileSchema
 
 def save_file(file: UploadFile) -> str:
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
-    with open(file_path, "wb+") as file_object:
-        file_object.write(file.file.read())
-    return file.filename
+
+    contents = file.read()
+    size = len(contents)
+    # Step 1: Real MIME type validation
+    mime = magic.from_buffer(contents, mime=True)
+    allowed_mimes = ["image/png", "image/jpeg", "image/jpg"]
+    
+    if mime not in allowed_mimes:
+         raise HTTPException(
+            status_code=400,
+            detail=f"Invalid content type: {mime}. Only image files are allowed."
+        )
+    
+    # Step 2: Pydantic validation (extension + size)
+    validated = FileSchema(filename=file.filename, size=size)
+    try:
+        # Step 3: Save file only if all validations passed
+        file_path = os.path.join(UPLOAD_DIR, file.filename)
+        with open(file_path, "wb+") as file_object:
+            file_object.write(contents)
+        return file.filename
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
 
 def list_files() -> list:
     return os.listdir(UPLOAD_DIR)
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,23 @@
-from pydantic  import BaseModel
+from pydantic  import BaseModel, constr, field_validator
 
 class FileSchema(BaseModel):
-    filename: str
+    filename: constr(strip_whitespace=True, min_length=3, max_length=255)
     url: str
+    size: int | None = None
+    type: str | None = None
+
+    @field_validator("filename")
+    @classmethod
+    def check_file_extension(cls, value):
+        allowed_ext = (".png", ".jpg", ".jpeg")
+        if not value.lower().endswith(allowed_ext):
+            raise ValueError("Only image files are allowed")
+        return value
+    
+    @field_validator("size")
+    @classmethod
+    def check_file_size(cls, value):
+        max_size = 2 * 1024 * 1024  # 2 MB
+        if value > max_size:
+            raise ValueError("File size exceeds 2 MB limit")
+        return value
